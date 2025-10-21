ceafda124f57a775ddf1aba60bdbbc11b4ed6b97
symmagency <agencysymm@gmail.com>
2025-10-21 16:05:23 -0300

    Setup inicial

---
+++ banners-mobile.js
@@ -0,0 +1,30 @@
+
+
+$(document).ready(function(){
+    
+    
+var tam = $(window).width();
+
+if (tam <= 767 ){
+    
+    //BANNER MOBILE
+
+    $('.banner.cheio .flexslider .slides>li:nth-child(1) img').attr('src','https://cdn.awsli.com.br/2760/2760817/arquivos/banner-mobile.png');
+    $('.banner.cheio .flexslider .slides>li:nth-child(2) img').attr('src','https://cdn.awsli.com.br/2760/2760817/arquivos/banner-mobile.png');
+    //$('.banner.cheio .flexslider .slides>li:nth-child(3) img').attr('src','https://cdn.awsli.com.br/1041/1041512/arquivos/ff-mobile.png');
+    //$('.banner.cheio .flexslider .slides>li:nth-child(4) img').attr('src','https://cdn.awsli.com.br/1041/1041512/arquivos/dragon-mobile.png');
+    //$('.banner.cheio .flexslider .slides>li:nth-child(5) img').attr('src','https://cdn.awsli.com.br/1041/1041512/arquivos/fornite-mobile.png');
+    //$('.banner.cheio .flexslider .slides>li:nth-child(6) img').attr('src','https://cdn.awsli.com.br/1041/1041512/arquivos/minecraft-mobile.png');
+    //$('.banner.cheio .flexslider .slides>li:nth-child(7) img').attr('src','https://cdn.awsli.com.br/1041/1041512/arquivos/gow-mobile.png');
+    //$('.banner.cheio .flexslider .slides>li:nth-child(8) img').attr('src','https://cdn.awsli.com.br/1041/1041512/arquivos/wukong-m.png');
+    //$('.banner.cheio .flexslider .slides>li:nth-child(9) img').attr('src','https://cdn.awsli.com.br/1041/1041512/arquivos/spider-man-mobile.png');
+    //$('.banner.cheio .flexslider .slides>li:nth-child(10) img').attr('src','https://cdn.awsli.com.br/1041/1041512/arquivos/xbox-cred.png');
+    //$('.banner.cheio .flexslider .slides>li:nth-child(11) img').attr('src','https://cdn.awsli.com.br/1041/1041512/arquivos/resident-evil-4-mobile.png');
+    //$('.banner.cheio .flexslider .slides>li:nth-child(12) img').attr('src','https://cdn.awsli.com.br/1041/1041512/arquivos/nioh-mobile.png');
+    
+    //FIM DO BANNER MOBILE
+    
+}
+
+});
+    
